f8fad02925ce8e79cb10e9ac47df91c56d7201b2
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2025-08-26 16:17:56 +0700

    Update Content Wrapper Background Color in Layout
    - Changed the background color of the content wrapper in the app layout to #FAFAFA for improved visual consistency across the application.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -454,7 +454,7 @@
         </aside>
 
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
+        <div class="content-wrapper" style="background-color: #FAFAFA !important;">
             <!-- Content Header (Page header) -->
             <div class="content-header">
                 <div class="container-fluid">
